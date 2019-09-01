bd28c36a4864ac6a3ce70fa760473da1697e3e2b
ImamS2 <sudiyatmoimam@gmail.com>
2019-09-01 12:14:19 +0700

    mengubah file base url untuk local

--- application/config/config.php
+++ application/config/config.php
@@ -1,5 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+
 date_default_timezone_set('Asia/Jakarta');
 
 /*
@@ -24,7 +25,9 @@ date_default_timezone_set('Asia/Jakarta');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = "https://dev.app-ithenticate.com";
+$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+$config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
+$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 
 /*
 |--------------------------------------------------------------------------
