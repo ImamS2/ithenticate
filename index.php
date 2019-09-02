1e3fded2a980eb561199386ee0815498a4730bf3
ImamS2 <sudiyatmoimam@gmail.com>
2019-09-02 17:47:21 +0700

    off debug env

--- index.php
+++ index.php
@@ -55,7 +55,7 @@
  */
 
 define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : $env);
-echo ($_SERVER["CI_ENV"]);
+// echo ($_SERVER["CI_ENV"]);
 /*
  *---------------------------------------------------------------
  * ERROR REPORTING
