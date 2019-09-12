24fce58485eb5e2a53e02182ec71bc56a3b63b2f
ImamS2 <sudiyatmoimam@gmail.com>
2019-09-12 17:13:54 +0700

    menambah fungsi get account campus untuk get folder api

--- application/modules/user/models/Account_campus_model.php
+++ application/modules/user/models/Account_campus_model.php
@@ -28,5 +28,18 @@ class Account_campus_model extends MY_Model
 	public function get_account_campus($id_user = NULL)
 	{
 		$id_user = isset($id_user) ? $id_user : $this->session->userdata("user_id");
+		$this->load->model("user/Group_model");
+		$user_campus = $this->Group_model->get_user_campus($id_new_user)->row();
+		$id_campus = $user_campus->id;
+		$this->load->model("api/Api_account_model");
+		$acc_active = $this->Api_account_model->where(array("active"=>1))->limit(1)->get();
+		if ($acc_active->num_rows() === 1) {
+			$id_acc = $acc_active->row()->id;
+			$this->where(array("id_campus"=>$id_campus,"id_account"=>$id_acc));
+		} else {
+			$this->where(array("id_campus"=>$id_campus));
+		}
+		$this->get();
+		return $this;
 	}
 }
