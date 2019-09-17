5bd9b680ba4cff1552e307d202b8e112643d2254
ImamS2 <sudiyatmoimam@gmail.com>
2019-09-17 16:11:09 +0700

    nambah

---
+++ application/config/email.php
@@ -0,0 +1,7 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['protocol'] = 'mail';
+$config['mailtype'] = 'html';
+$config['charset'] = 'utf-8';
+$config['wordwrap'] = TRUE;
